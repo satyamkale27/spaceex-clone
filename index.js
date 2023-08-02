0cb02a0e3bddc34aa13d7f77cd92311c2929b328
satyam kale <satyamkale271@gmail.com>
2023-08-02 14:32:41 +0530

    incomplete responsive using only css(media query) and js

---
+++ index.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector('.burger')
+let bar = document.querySelector('.list-responsive')
+
+    burger.addEventListener('click', ()=>{
+        bar.classList.toggle('vsble');
+    })
